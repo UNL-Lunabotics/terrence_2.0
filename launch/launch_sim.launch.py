b3c1d24f5abced1cacd6909ca0a8b59e30c78096
Jesse Mills <millsj255@gmail.com>
2025-11-07 17:29:27 -0600

    updated launch file to launch ros_control and utilize additional parameters

--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -28,6 +28,8 @@ def generate_launch_description():
         launch_arguments={"use_sim_time": "true"}.items(),
     )
 
+    gazebo_params_file = os.path.join(get_package_share_directory(package_name), "config", "gazebo_params.yaml")
+
     # Include the Gazebo launch file, provided by the gazebo_ros package
     gazebo = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
@@ -39,6 +41,7 @@ def generate_launch_description():
                 )
             ]
         ),
+        launch_arguments={"extra_gazebo_args": "--ros-args --params-file " + gazebo_params_file}.items(),
     )
 
     # Run the spawner node from the gazebo_ros package. The entity name doesn't really matter if you only have a single robot.
@@ -48,6 +51,18 @@ def generate_launch_description():
         arguments=["-topic", "robot_description", "-entity", "my_bot"],
         output="screen",
     )
+    
+    diff_drive_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["diff_cont"],
+    )
+
+    joint_broad_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["joint_broad"],
+    )
 
     # Launch them all!
     return LaunchDescription(
@@ -55,5 +70,7 @@ def generate_launch_description():
             rsp,
             gazebo,
             spawn_entity,
+            diff_drive_spawner,
+            joint_broad_spawner
         ]
     )
