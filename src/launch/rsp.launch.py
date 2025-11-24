464ad58d270165bac1824b6e9cb5e3f5d9106af0
Aiden Kimmerling <apkimmerling@gmail.com>
2025-11-23 22:08:56 -0600

    Initial conversion to actual terrence
    NOTE: ros2_control does NOT work yet

--- src/launch/rsp.launch.py
+++ src/launch/rsp.launch.py
@@ -4,6 +4,7 @@ from ament_index_python.packages import get_package_share_directory
 
 from launch import LaunchDescription
 from launch.substitutions import LaunchConfiguration, Command
+from launch_ros.parameter_descriptions import ParameterValue
 from launch.actions import DeclareLaunchArgument
 from launch_ros.actions import Node
 
@@ -13,18 +14,24 @@ def generate_launch_description():
 
     # Check if we're told to use sim time
     use_sim_time = LaunchConfiguration('use_sim_time')
-    use_ros2_control = LaunchConfiguration('use_ros2_control')
 
     # Process the URDF file
     pkg_path = os.path.join(get_package_share_directory('terrence_2'))
-    xacro_file = os.path.join(pkg_path, 'description', 'robot.urdf.xacro')
-    robot_description_config = Command(['xacro ', xacro_file, ' use_ros2_control:=', use_ros2_control, ' sim_mode:=', use_sim_time])
+    xacro_file = os.path.join(pkg_path, 'description', 'terrence.urdf.xacro')
+    robot_description_config = Command(['xacro ', xacro_file, ' sim_mode:=', use_sim_time])
     
     # Create a robot_state_publisher node
     params = {'robot_description': robot_description_config, 'use_sim_time': use_sim_time}
-    node_robot_state_publisher = Node(
+    robot_state_publisher = Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
+        output='screen',
+        parameters=[params]
+    )
+    
+    joint_state_publisher = Node(
+        package='joint_state_publisher',
+        executable='joint_state_publisher',
         output='screen',
         parameters=[params]
     )
@@ -40,5 +47,6 @@ def generate_launch_description():
             default_value='true',
             description='Use ros2_control if true'),
 
-        node_robot_state_publisher
+        robot_state_publisher,
+        joint_state_publisher
     ])
