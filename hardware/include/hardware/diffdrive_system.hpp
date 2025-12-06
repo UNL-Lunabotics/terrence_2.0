2168c63844119d9e59ce24db22d55c455c2184b4
TheThingKnownAsKit <moodyellam@gmail.com>
2025-12-06 00:25:27 +0000

    moved hardware up a layer and fixed the cmake

--- hardware/include/hardware/diffdrive_system.hpp
+++ hardware/include/hardware/diffdrive_system.hpp
@@ -29,10 +29,10 @@
 #include "rclcpp/time.hpp"
 #include "rclcpp_lifecycle/node_interfaces/lifecycle_node_interface.hpp"
 #include "rclcpp_lifecycle/state.hpp"
-#include "diffdrive_arduino/visibility_control.h"
+#include "hardware/visibility_control.h"
 
-#include "diffdrive_arduino/arduino_comms.hpp"
-#include "diffdrive_arduino/wheel.hpp"
+#include "hardware/arduino_comms.hpp"
+#include "hardware/wheel.hpp"
 
 namespace diffdrive_arduino
 {
--- hardware/src/diffdrive_system.cpp
+++ hardware/src/diffdrive_system.cpp
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include "diffdrive_arduino/diffbot_system.h"
+#include "hardware/diffdrive_system.hpp"
 
 #include <chrono>
 #include <cmath>
