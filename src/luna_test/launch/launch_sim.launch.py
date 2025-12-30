3a26639db982a2d28ba195318e14629c25dc071e
Jesse Mills <millsj255@gmail.com>
2025-12-30 18:29:01 +0000

    Finished controller input

--- src/luna_test/launch/launch_sim.launch.py
+++ src/luna_test/launch/launch_sim.launch.py
@@ -40,23 +40,30 @@ def generate_launch_description():
                                    '-entity', 'my_bot'],
                         output='screen')
 
-    diff_drive_spawner = Node(
-        package='controller_manager',
-        executable='spawner',
-        arguments=['diff_cont'],
-    )
-    
     joint_broad_spawner = Node(
         package='controller_manager',
         executable='spawner',
         arguments=['joint_broad'],
     )
 
+    diff_drive_spawner = Node(
+        package='controller_manager',
+        executable='spawner',
+        arguments=['diff_cont'],
+    )
+    
+    joystick_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            [os.path.join(get_package_share_directory(package_name),'launch','joystick.launch.py')]
+        )
+    )
+    
     # Launch them all!
     return LaunchDescription([
         rsp,
         gazebo,
         spawn_entity,
+        joint_broad_spawner,
         diff_drive_spawner,
-        joint_broad_spawner
+        joystick_launch
     ])
